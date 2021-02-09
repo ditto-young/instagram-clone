e043b65f2c5c0697fc30f827f3e81b8c2293589e
gpdud0243 <gpdud0243@naver.com>
2021-02-09 23:58:10 +0900

    new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Post from './Post';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username:"hiyoung",
+      caption:"Wow it works",
+      imageUrl:"https://www.freecodecamp.org/news/content/images/size/w1000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+    },
+    {
+      username:"hiyoung",
+      caption:"Wow it works",
+      imageUrl:"https://www.freecodecamp.org/news/content/images/size/w1000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+    }
+  ]);
+
+  useEffect(() => {
+    //this is where the code runs
+  }, [posts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+          alt=""
+          />
+      </div>
+
+      <h1>Hello let's build and instagram clone with react</h1>
+      
+      {
+        posts.map(post => (
+          <Post usename={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
+      }
+
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAdeWqR7jphCktyW0XNq-N3faPiFLV2vlw",
+    authDomain: "instagram-clone-react-c4a39.firebaseapp.com",
+    databaseURL: "https://instagram-clone-react-c4a39-default-rtdb.firebaseio.com",
+    projectId: "instagram-clone-react-c4a39",
+    storageBucket: "instagram-clone-react-c4a39.appspot.com",
+    messagingSenderId: "759709830507",
+    appId: "1:759709830507:web:fc95b8bd258cdd0e2d902e",
+    measurementId: "G-9BKZBTEKSN"
+  });
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const storage = firebase.storage();
+
+  export { db, auth, storage };
+  //export default firebaseConfig;
